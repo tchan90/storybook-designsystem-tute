0eead51b92dc0a01d47a0afb215a616d11fd0bc4
tchan90 <47408197+tchan90@users.noreply.github.com>
2020-09-30 14:55:25 +1000

    change to avatar size

--- src/Avatar.js
+++ src/Avatar.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
-import { color, typography } from './shared/styles';
-import { glow } from './shared/animation';
-import { Icon } from './Icon';
+import React from "react";
+import PropTypes from "prop-types";
+import styled, { css } from "styled-components";
+import { color, typography } from "./shared/styles";
+import { glow } from "./shared/animation";
+import { Icon } from "./Icon";
 
 export const sizes = {
-  large: 40,
-  medium: 28,
-  small: 20,
-  tiny: 16,
+	large: 40,
+	medium: 28,
+	small: 20,
+	tiny: 14,
 };
 
 const Image = styled.div`
-  background: ${props => (!props.loading ? 'transparent' : color.light)};
+  background: ${(props) => (!props.loading ? "transparent" : color.light)};
   border-radius: 50%;
   display: inline-block;
   vertical-align: top;
@@ -24,35 +24,35 @@ const Image = styled.div`
   width: ${sizes.medium}px;
   line-height: ${sizes.medium}px;
 
-  ${props =>
-    props.size === 'tiny' &&
-    css`
-      height: ${sizes.tiny}px;
-      width: ${sizes.tiny}px;
-      line-height: ${sizes.tiny}px;
-    `}
+  ${(props) =>
+		props.size === "tiny" &&
+		css`
+			height: ${sizes.tiny}px;
+			width: ${sizes.tiny}px;
+			line-height: ${sizes.tiny}px;
+		`}
 
-  ${props =>
-    props.size === 'small' &&
-    css`
-      height: ${sizes.small}px;
-      width: ${sizes.small}px;
-      line-height: ${sizes.small}px;
-    `}
+  ${(props) =>
+		props.size === "small" &&
+		css`
+			height: ${sizes.small}px;
+			width: ${sizes.small}px;
+			line-height: ${sizes.small}px;
+		`}
 
-  ${props =>
-    props.size === 'large' &&
-    css`
-      height: ${sizes.large}px;
-      width: ${sizes.large}px;
-      line-height: ${sizes.large}px;
-    `}
+  ${(props) =>
+		props.size === "large" &&
+		css`
+			height: ${sizes.large}px;
+			width: ${sizes.large}px;
+			line-height: ${sizes.large}px;
+		`}
 
-  ${props =>
-    !props.src &&
-    css`
-      background: ${!props.loading && '#37D5D3'};
-    `}
+  ${(props) =>
+		!props.src &&
+		css`
+			background: ${!props.loading && "#37D5D3"};
+		`}
 
   img {
     width: 100%;
@@ -104,52 +104,52 @@ const Initial = styled.div`
  *   the avatar or in a tooltip.
  **/
 export function Avatar({ loading, username, src, size, ...props }) {
-  let avatarFigure = <Icon icon="useralt" />;
-  const a11yProps = {};
+	let avatarFigure = <Icon icon="useralt" />;
+	const a11yProps = {};
 
-  if (loading) {
-    a11yProps['aria-busy'] = true;
-    a11yProps['aria-label'] = 'Loading avatar ...';
-  } else if (src) {
-    avatarFigure = <img src={src} alt={username} />;
-  } else {
-    a11yProps['aria-label'] = username;
-    avatarFigure = (
-      <Initial size={size} aria-hidden="true">
-        {username.substring(0, 1)}
-      </Initial>
-    );
-  }
+	if (loading) {
+		a11yProps["aria-busy"] = true;
+		a11yProps["aria-label"] = "Loading avatar ...";
+	} else if (src) {
+		avatarFigure = <img src={src} alt={username} />;
+	} else {
+		a11yProps["aria-label"] = username;
+		avatarFigure = (
+			<Initial size={size} aria-hidden="true">
+				{username.substring(0, 1)}
+			</Initial>
+		);
+	}
 
-  return (
-    <Image size={size} loading={loading} src={src} {...a11yProps} {...props}>
-      {avatarFigure}
-    </Image>
-  );
+	return (
+		<Image size={size} loading={loading} src={src} {...a11yProps} {...props}>
+			{avatarFigure}
+		</Image>
+	);
 }
 
 Avatar.propTypes = {
-  /**
+	/**
    Use the loading state to indicate that the data Avatar needs is still loading. 
   */
-  loading: PropTypes.bool,
-  /**
+	loading: PropTypes.bool,
+	/**
    Avatar falls back to the user’s initial when no image is provided. Supply a `username` and omit `src` to see what this looks like.
   */
-  username: PropTypes.string,
-  /**
+	username: PropTypes.string,
+	/**
    The URL of the Avatar's image.
   */
-  src: PropTypes.string,
-  /**
+	src: PropTypes.string,
+	/**
    Avatar comes in four sizes. In most cases, you’ll be fine with `medium`. 
   */
-  size: PropTypes.oneOf(Object.keys(sizes)),
+	size: PropTypes.oneOf(Object.keys(sizes)),
 };
 
 Avatar.defaultProps = {
-  loading: false,
-  username: 'loading',
-  src: null,
-  size: 'medium',
+	loading: false,
+	username: "loading",
+	src: null,
+	size: "medium",
 };
